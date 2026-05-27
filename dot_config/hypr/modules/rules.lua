633315508b10c4185b6507fa30fcec40629a4a18
sabisho <ayushrajmth91@gmail.com>
2026-05-27 17:19:06 +0530

    Added layerrules

--- dot_config/hypr/modules/rules.lua
+++ dot_config/hypr/modules/rules.lua
@@ -1,16 +1,24 @@
--- modules/rules.lua
--- https://wiki.hypr.land/Configuring/Basics/Window-Rules/
+-- Layer Rules for wayland layers.
+hl.layer_rule({
+	name = "noctalia",
+	match = {
+		namespace = "noctalia-background-.*$",
+	},
+	ignore_alpha = 0.5,
+	blur = true,
+	blur_popups = true,
+})
 
--- ignore maximize requests from all apps
+-- Hyprland window rules
 hl.window_rule({
-    name           = "suppress-maximize",
-    match          = { class = ".*" },
-    suppress_event = "maximize",
+	name = "suppress-maximize",
+	match = { class = ".*" },
+	suppress_event = "maximize",
 })
 
 -- fix XWayland drag issues
 hl.window_rule({
-    name     = "fix-xwayland-drag",
-    match    = { class = "^$", title = "^$", xwayland = true, float = true, fullscreen = false, pin = false },
-    no_focus = true,
+	name = "fix-xwayland-drag",
+	match = { class = "^$", title = "^$", xwayland = true, float = true, fullscreen = false, pin = false },
+	no_focus = true,
 })
