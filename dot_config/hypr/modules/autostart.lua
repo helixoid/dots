3081b37ff57826c67e9e983e51d66a47daecdc18
sabisho <ayushrajmth91@gmail.com>
2026-05-27 20:54:40 +0530

    Added noctalia fixes!

--- dot_config/hypr/modules/autostart.lua
+++ dot_config/hypr/modules/autostart.lua
@@ -3,6 +3,7 @@
 -- https://wiki.hypr.land/Configuring/Basics/Autostart/
 
 hl.on("hyprland.start", function()
-    hl.exec_cmd("cliphist wipe")
-    hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+	hl.exec_cmd("qs -c noctalia-shell")
+	hl.exec_cmd("cliphist wipe")
+	hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 end)
