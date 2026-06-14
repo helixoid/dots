1f6f4299b762ebd99c12a37f72c461b427fb8399
helixoid <ayushrajmth91@gmail.com>
2026-06-15 03:01:24 +0530

    Moved to vim.pack for nvim plugins management.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,5 +1,13 @@
 -- [[ Setting the leader key ]]
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-require 'core'
+require("keymaps")
+require("options")
+require("plugins.colorscheme")
+require("plugins.completions")
+require("plugins.fff")
+require("plugins.formatter")
+require("plugins.lsp")
+require("plugins.snacks")
+require("plugins.treesitter")
